0e5061f61407049fe8bb1fd6dd18273a151b9a56
Julien Rivard <juriv33@ulaval.ca>
2023-12-28 14:24:27 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  // https://www.youtube.com/watch?v=yo2bMGnIKE8 1:40
+  base: '/calculateur-pate/',
+
   plugins: [
     vue(),
   ],
