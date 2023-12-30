676d0eef793f18fcabb3e98352d55f23442ab979
Julien Rivard <juriv33@ulaval.ca>
2023-12-29 22:55:33 -0500

    Beta 0.1

--- dist/assets/AccueilView-rrau45uH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,a as t}from"./index-n-YQc3m-.js";const n={},s=t("h2",null,"Accueil",-1),a=[s];function _(r,l){return c(),o("main",null,a)}const u=e(n,[["render",_]]);export{u as default};
