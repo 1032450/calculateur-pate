adc4e6a365106f21e86c4183108504849fe965c9
Julien Rivard <juriv33@ulaval.ca>
2023-12-30 19:37:27 -0500

    Beta 0.3 : Couleur menu, bouton X pour effacer, accueil.

--- assets/AccueilView-Olw3K_R5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,a as t}from"./index-TZqbvgz3.js";const n={},s=t("h2",null,"Accueil",-1),a=[s];function _(r,l){return c(),o("main",null,a)}const u=e(n,[["render",_]]);export{u as default};
