c9b21bb48f2be64e0d86664d91dceda8e94118a4
Julien Rivard <juriv33@ulaval.ca>
2024-01-06 21:00:53 -0500

    Beta 0.5 : utilise un serveur en ligne.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import {store} from "@/store.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: store.routes
+  routes: [
+    {
+      path: "/",
+      name: "Accueil",
+      component: () => import('../views/AccueilView.vue')
+    },
+    {
+      path: "/:pate",
+      name: "Pate",
+      component: () => import ('../views/CalculateurPate.vue')
+    }
+  ]
 })
 
 export default router
